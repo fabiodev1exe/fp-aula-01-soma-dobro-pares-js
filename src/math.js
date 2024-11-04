3bc44e814565ed9e9c46f42dc6cb932ff67b08b0
gumaadev <145726819+fabiodev1exe@users.noreply.github.com>
2024-11-04 16:32:59 -0300

    Update math.js

--- src/math.js
+++ src/math.js
@@ -1,12 +1,3 @@
-/**
- * Módulo para implementar funções matemáticas.
- */
-
-/**
- * Realiza a soma do dobro do valor de cada número par em um vetor de números.
- * A função deve ser implementada utilizando a abordagem funcional com o modelo map/reduce.
- * @param {Array} numeros vetor com os números desejados
- */
 function somaDobroNumerosParesFuncional(numeros) {
     return numeros
         .filter(num => num % 2 === 0)
@@ -14,11 +5,6 @@ function somaDobroNumerosParesFuncional(numeros) {
         .reduce((acc, num) => acc + num, 0); 
 }
 
-/**
- * Realiza a soma do dobro do valor de cada número par em um vetor de números.
- * A função deve ser implementada utilizando a abordagem estruturada (procedural) convencional.
- * @param {Array} numeros vetor com os números desejados
- */
 function somaDobroNumerosParesProcedural(numeros) {
     let soma = 0;
     for (let i = 0; i < numeros.length; i++) {
